0e90b17bfa21d006e82be8459aad45132ea8a258
Krzem <krzem5.dev@gmail.com>
2021-01-12 20:26:29 +0100

    Push Update 01/12/2021, 20:03:43

--- index.bat
+++ index.bat
@@ -1,5 +1,21 @@
-echo off
-echo NUL>_.class&&del /s /f /q *.class
+@echo off
 cls
-javac com/krzem/screen_blocker/Main.java&&java com/krzem/screen_blocker/Main
-start /min cmd /c "echo NUL>_.class&&del /s /f /q *.class"
+if exist build rmdir /s /q build
+mkdir build
+cd src
+javac -d ../build com/krzem/screen_blocker/Main.java&&jar cvmf ../manifest.mf ../build/screen_blocker.jar -C ../build *&&goto run
+cd ..
+goto end
+:run
+cd ..
+pushd "build"
+for /D %%D in ("*") do (
+	rd /S /Q "%%~D"
+)
+for %%F in ("*") do (
+	if /I not "%%~nxF"=="screen_blocker.jar" del "%%~F"
+)
+popd
+cls
+java -jar build/screen_blocker.jar
+:end
