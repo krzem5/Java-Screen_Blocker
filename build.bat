fbacd4fd12e9dc9a986a327ec1d152150d1d9b7c
Krzem <krzem5.dev@gmail.com>
2021-04-10 21:14:33 +0200

    Push Update 04/10/2021, 21:14:26

--- build.bat
+++ build.bat
@@ -1,21 +1,3 @@
 @echo off
 cls
-if exist build rmdir /s /q build
-mkdir build
-cd src
-javac -d ../build com/krzem/screen_blocker/Main.java&&jar cvmf ../manifest.mf ../build/screen_blocker.jar -C ../build *&&goto run
-cd ..
-goto end
-:run
-cd ..
-pushd "build"
-for /D %%D in ("*") do (
-	rd /S /Q "%%~D"
-)
-for %%F in ("*") do (
-	if /I not "%%~nxF"=="screen_blocker.jar" del "%%~F"
-)
-popd
-cls
-java -jar build/screen_blocker.jar
-:end
+python build.py %*
